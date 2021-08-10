d686f0daf7209ff38a76d0e38cd423e862eabdc7
Sander-317 <sander.van.meerveld@gmail.com>
2021-08-10 19:24:05 +0200

    made zodiac signs work

--- script.js
+++ script.js
@@ -59,26 +59,52 @@ function getAllTheFemalesOlderThan30(array) {
 }
 
 function isZodiacsignSteenbok(array) {
-  const steenbokStartDate = new Date(12-22)
-  const steenbokEndDate = new Date(1-19)
- const isSteenbok = array.map((item) => {
+  const isSteenbok = array.map((item) => {
    const splitBirthday = item.birthday.mdy.split("/")
    const day = splitBirthday[1]
    const month = splitBirthday[0]
-   const dayMonth = new Date(`${month}`-`${day}`)
-   console.log("dit is daymonth",dayMonth)
-   const steenbok = dateCheck(dayMonth, steenbokStartDate,steenbokEndDate)
-   if (steenbok === true)
-   console.log(steenbok)
-   console.log(typeof steenbok)
-   return console.log(item)
-  
+   if ((month == 1 && day <= 20) || (month == 12 && day >= 22))
+    return item
+  // else
+  //  return 
+   
  })
-console.log(`isSteenbok =${isSteenbok}`)
+console.log(isSteenbok)
+const newArray = removeUndefinedFromArray(isSteenbok)
+console.log(newArray)
 }
+
+function removeUndefinedFromArray(array){
+  const filteredArray = array.filter( (item) => {
+          return  item !== undefined
+  })
+  return filteredArray
+}
+
+
+
 // (date, min, max) => (dayMonth.getTime() >= steenbokStartDate.getTime() && dayMonth.getTime() <= steenbokEndDate.getTime())
-function dateCheck(date, min, max){
-   if (date.getTime() >= min.getTime() && date.getTime() <= max.getTime()) 
-    return true
-  else return false
-}
+// function dateCheck(date, min, max){
+//    if (date.getTime() >= min.getTime() && date.getTime() <= max.getTime()) 
+//     return true
+//   else return false
+// }
+
+// function isZodiacsignSteenbok(array) {
+//   const steenbokStartDate = new Date(12-22)
+//   const steenbokEndDate = new Date(1-19)
+//  const isSteenbok = array.map((item) => {
+//    const splitBirthday = item.birthday.mdy.split("/")
+//    const day = splitBirthday[1]
+//    const month = splitBirthday[0]
+//    const dayMonth = new Date(`${month}`-`${day}`)
+//    console.log("dit is daymonth",dayMonth)
+//    const steenbok = dateCheck(dayMonth, steenbokStartDate,steenbokEndDate)
+//    if (steenbok === true)
+//    console.log(steenbok)
+//    console.log(typeof steenbok)
+//    return console.log(item)
+  
+//  })
+// console.log(`isSteenbok =${isSteenbok}`)
+// }
